326498904856e0e25b32735dbdff52cd88ac70bc
Brendan Woo <woo.brendan@gmail.com>
2023-05-18 16:26:41 -0500

    import dotenv and os, use  os.getenv for secret key

--- backend/backend_server/backend_server/settings.py
+++ backend/backend_server/backend_server/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i_^*tkwd(!wezk%q-p%2!9hk42*1s41v3*&&j!suy(dt3+_=hy'
+SECRET_KEY = os.getenv('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
